8e7ec1487ac938ff36bb758b6e18f2d1b1780e3b
Mojert <hamza.meel@yahoo.fr>
2017-08-03 12:41:53 +0200

    Ch 6 : Les éléments d'options

--- Ch6/js/cours.js
+++ Ch6/js/cours.js
@@ -14,4 +14,25 @@ pseudoElt.addEventListener("blur", function () {
 // Il est possible de chaner le focus de puis le JS. pseudoElt.focus() et .blur()
 // C'est exactement le même principe pour les champs à plusieurs lignes (balise <textarea>)
 
+// Les éléments d'options
+// Cases à cocher
+// L'élément possède une propriété checked. Un événement "change" est envoyé dès que cette valeure est changée par l'utilisateur
+document.getElementById("confirmation").addEventListener("change", (e) => {
+    console.log("Demande de confirmation : " + e.target.checked);
+});
+// Boutons radio
+// Pour avoir un groupe de boutons radio, il faut qu'ils aient tous le même "name" et qu'ils possèdent des "value" diffèrentes
+// Affichage du type d'abonnement choisi
+var aboElts = document.getElementsByName("abonnement");
+for (var i = 0; i < aboElts.length; i++) {
+    aboElts[i].addEventListener("change", function (e) {
+        // L'événement "change" est envoyé seulement si le bouton devient le bouton sélectionné
+        console.log("Formule d'abonnement choisie : " + e.target.value);
+    });
+}
+// Liste déroulante
+document.getElementById("nationalite").addEventListener("change", (e) => {
+    // La proptiété value de l'élément <select> renvoit l'attribut value du <option> sélectionné
+    console.log("Code de nationalité : " + e.target.value);
+})
 
