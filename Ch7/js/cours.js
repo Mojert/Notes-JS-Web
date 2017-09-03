92e321d7de3f09fcdaeeb93e6ee75ab473bd7c0d
Mojert <hamza.meel@yahoo.fr>
2017-09-03 11:49:38 +0200

    Ch7 : Effectuer une action après un délai

--- Ch7/js/cours.js
+++ Ch7/js/cours.js
@@ -11,10 +11,15 @@ function diminuerCompteur() {
         // Modifie le titre de la page
         var titreElt = document.getElementById("titre");
         titreElt.textContent = "BOUM !!!";
+        // Modification du titre au bout de 2 secondes
+        setTimeout(() => {
+            titreElt.textContent = "Tout est cassé :(";
+        }, 2000);
     }
 }
 
 // setInterval est utilisée pour répéter une action périodiquement
+// setTimeout permet d'exécuter une fonction une seule fois après un certain délai
 
 // Apelle la fonction diminuerCompteur toutes les 1000 milisecondes
 var intervalID = setInterval(diminuerCompteur, 1000); // La fonction renvoie un nombre servant à identifier l'intervalle
