a288c6e1a5e5f9419cfe947b91c77a34078c3eb0
Mojert <hamza.meel@yahoo.fr>
2017-09-06 17:01:33 +0200

    Ch9 : cours terminé

--- Ch9/js/cours.js
+++ Ch9/js/cours.js
@@ -14,3 +14,37 @@ function ajouterArticles(articlesStr){
 }
 
 ajaxGet("https://oc-jswebsrv.herokuapp.com/api/articles", ajouterArticles);
+
+
+var premMinElt = document.getElementById("premMin");
+// Accès aux informations publiques sur le Premier Ministre
+ajaxGet("https://www.data.gouv.fr/api/1/organizations/premier-ministre/", (reponse) => {
+    var premMin = JSON.parse(reponse);
+    // Texte
+    var texteElt = document.createElement("p");
+    texteElt.textContent = premMin.description;
+    // Image
+    var imageElt = document.createElement("img");
+    imageElt.src = premMin.logo_thumbnail;
+
+    premMinElt.appendChild(texteElt);
+    premMinElt.appendChild(imageElt);
+});
+
+var meteoElt = document.getElementById("meteo");
+
+ajaxGet("http://api.wunderground.com/api/" + weatherUnderground + "/conditions/q/Switzerland/Lausanne.json", (reponse) => {
+    var meteo = JSON.parse(reponse);
+    // Récupération de certains résultats
+    var temperature = meteo.current_observation.temp_c;
+    var humidite = meteo.current_observation.relative_humidity;
+    var imageUrl = meteo.current_observation.icon_url;
+    // Affichage des résultats
+    var conditionElt = document.createElement("p");
+    conditionElt.textContent = "Il fait actuellement " + temperature + "°C et l'humidité est de " + humidite;
+    var imageElt = document.createElement("img");
+    imageElt.src = imageUrl;
+
+    meteoElt.appendChild(conditionElt);
+    meteoElt.appendChild(imageElt);
+});
